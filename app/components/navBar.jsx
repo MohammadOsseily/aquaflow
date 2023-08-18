cca6fe7ac80a5f0a4535aff2bc762c07bd9b7e8a
mohammad.osseily <mohammad.osseily2@gmail.com>
2023-08-18 14:03:41 +0300

    the navbar design bones done

---
+++ app/components/navBar.jsx
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.css';
+function navBar(){
+    return(
+        <>
+
+                <div className ="d-flex justify-content-between">
+                    <div className="d-flex">Logo</div>
+
+                    <div className="d-flex">
+                        <div className="p-4">Products</div>
+                        <div className="p-4">About</div>
+                        <div className="p-4">App</div>
+                    </div>
+
+                    <div className="d-flex">
+                        <div className="p-4">login</div>
+                        <div className="p-4">register</div>
+                    </div>
+
+                </div>
+
+
+        </>
+
+)
+}
+
+
+export default navBar;
