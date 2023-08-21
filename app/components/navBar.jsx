e8aefa1a41888b8d0119bf19130f5f41bb341a27
mohammad.osseily <mohammad.osseily2@gmail.com>
2023-08-22 01:36:18 +0300

    added a new component Button.jsx designed the login and sign up button create neumorphism effect class

--- app/components/navBar.jsx
+++ app/components/navBar.jsx
@@ -1,26 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import Image from "next/image";
+import img from "/home/mhmd/geekgalaxy/public/images/logo.png"
+import styles from "@/styles/navBar.module.css"
+import Link from "next/link";
+import Button from "./Button"
+import themes from "@/styles/Button.module.css"
+
 function navBar(){
     return(
         <>
 
-                <div className ="d-flex justify-content-between">
-                    <div className="d-flex">Logo</div>
+            <div className= {` ${styles.border} d-flex justify-content-between w-75 mx-auto`}>
 
 
-
-                    <div className="d-flex">
-                        <div className="p-4">Products</div>
-                        <div className="p-4">About</div>
-                        <div className="p-4">App</div>
+                    <div className="pt-2 d-flex">
+                        <Image width={80} height={60} src={"/images/logo.png"} alt={"logo"}></Image>
                     </div>
 
                     <div className="d-flex">
-                        <div className="p-4">login</div>
-                        <div className="p-4">register</div>
+                        <div className={` ${styles.menulinks} p-4`}><Link className={` ${styles.menulinks}`} href="#">Products</Link></div>
+                        <div className={` ${styles.menulinks} p-4`}><Link className={` ${styles.menulinks}`} href="#">About</Link></div>
+                        <div className={` ${styles.menulinks} p-4`}><Link className={` ${styles.menulinks}`} href="#">Contact us</Link></div>
+                        <div className={` ${styles.menulinks} p-4`}><Link className={` ${styles.menulinks}`} href="#">FAQ</Link></div>
                     </div>
 
-                </div>
+                    <div className={`d-flex align-items-center rounded-pill ${styles.div_button} ${themes.neumorphism}`} >
+                        <Button title="Login" theme={themes.btn_white}/>
+                        <Button title="Sign Up" theme={themes.btn_black}/>
+                    </div>
 
+
+
+            </div>
 
         </>
 
--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 import Image from 'next/image'
-import styles from './page.module.css'
 import  NavBar from "./components/navBar"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-
+    <main >
+      <div >
           <div class ="Mobile">
               <h1>Mobile</h1>
           </div>
