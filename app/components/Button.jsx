9c109132675749ca13e234023b44061e2d51f5a1
mohammad.osseily <mohammad.osseily2@gmail.com>
2023-09-04 02:24:21 +0300

    fixing

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,3 +1,4 @@
+'use client'
 import styles from "@/styles/Button.module.css"
 import Image from "next/image";
 function Button(props){
--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,3 +1,4 @@
+'use client'
 import styles from "/styles/Card.module.css"
 import Image from "next/image";
 import CartButton from "@/app/components/CartButton";
