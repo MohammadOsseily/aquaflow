1520f0ab4ef331ba943578c7093b467558cedb49
mohammad.osseily <mohammad.osseily2@gmail.com>
2023-09-06 03:14:58 +0300

    finalized responsive product cart unable multi usage component

--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from "/styles/ProductCard.module.css"
 import Image from "next/image";
-import { PiShoppingCartThin } from 'react-icons/pi';
+import { BiSolidCart } from 'react-icons/bi';
 import Button from "./Button"
 import themes from "@/styles/Button.module.css"
 
@@ -11,27 +11,29 @@ function ProductCard(props){
 
     return(
         <>
+            <div className={" p-2 col-xs-6 col-md-6 col-lg-4 col-xl-3 "}>
+                <div className={`${styles.card}  neumorphism g-1 `} style={{height : "350px"}}>
 
-            <div className={`${styles.card} neumorphism w-25 card-deck `} style={{height : "350px"}}>
+                    <div className={`card d-flex flex-column bd-highlight mb-3`} style={{ backgroundColor: "transparent", paddingTop: "10px", border: "0px" ,positon:"relative"}}>
 
-                <div className={`card d-flex flex-column bd-highlight mb-3`} style={{width: "18rem", backgroundColor: "transparent", paddingTop: "10px", border: "0px"}}>
-
-                    <Image className={` ${styles.img} bd-highlight`} width={200} height={200} src={"/images/black-bottle.png"} styles={{backgroundColor: "transparent", marginLeft:"30px"}} />
-                        <div className="bg-transparent  bd-highlight">
-                            <div className={`${styles.text} bg-transparent d-flex justify-content-center bd-highlight my-4  mx-suto h-100`}>black bottle</div>
-                            <div className={` bg-transparent d-flex justify-content-center`}>
-                                <div className={`d-flex align-items-center rounded-pill ${styles.div_button} neumorphism`} >
-                                    <Button title="45$" theme={themes.btn_white}/>
-                                    <Button title={<PiShoppingCartThin size={30} style={{transform : "scaleX(-1)", marginLeft:"5px", height : "100%", backgroundColor: "transparent"}}/>} theme={themes.btn_black}/>
+                        <Image className={` ${styles.img} mx-auto  bd-highlight`} fill src={props.img}/>
+                        <div className="card-content bg-transparent   bd-highlight">
+                            <div className={`${styles.text} bg-transparent d-flex justify-content-center bd-highlight my-4  `}>{props.productName}</div>
+                            <div className={` bg-transparent d-flex justify-content-center`} >
+                                <div className={`d-flex align-items-center rounded-pill ${styles.div_button} neumorphism`} style={{width:"70%", height:"100%"}} >
+                                    <Button title={props.price} theme={themes.btn_productw}/>
+                                    <Button title={<BiSolidCart size="1.5em" style={{transform : "scaleX(-1)", marginLeft:"5px", height : "100%", backgroundColor: "transparent"}}/>} theme={themes.btn_productb}/>
                                 </div>
 
                             </div>
 
 
                         </div>
-                </div>
+                    </div>
 
+                </div>
             </div>
+
 
 
         </>
