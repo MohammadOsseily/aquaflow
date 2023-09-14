3f6d3ef5283c1ac348731d2121d1036a48e627a0
mohammad.osseily <mohammad.osseily2@gmail.com>
2023-09-14 03:10:28 +0300

    finalizing product detail button edited product details

--- app/productDetailes/page.jsx
+++ app/productDetailes/page.jsx
@@ -1,20 +1,32 @@
 import Image from "next/image";
 import styles from "/styles/ProductDetailes.module.css"
 import Link from "next/link";
+import Button from "../components/Button"
+
+
 
 
 function ProductDetailes(){
     return(
-        <div className={`${styles.container} mx-auto`}>
+        <div className={`${styles.container} mt-4 mx-auto`}>
             <Image className={`${styles.productImg}`} src="/images/black-bottle.png" width={550} height={550} alt="bottle"/>
             <div className={`${styles.productDetails}`}>
                 <div className={` ${styles.productText} bg-transperant`}>Lorem Ipsum is simply dummy</div>
                 <Link className={` ${styles.companyName}`} href="#">sony</Link>
                 <hr />
-                <div className={styles.smText}>Price:</div>
-                <div className={styles.smText}>Quantity:</div>
-                <div>
+                <div className={`${styles.product} d-flex justify-content-between bg-transperant`}>
+                    <div className={` ${styles.smText}`}>Price:</div>
+                    <div className={`${styles.productPrice} bg-transperant`}> 70$</div>
+                </div>
 
+
+                <div className={`${styles.product} d-flex justify-content-between bg-transperant mt-4 mb-4`}>
+                    <div className={` ${styles.smText} mt-3`}>Quantity:</div>
+                    <div className={`d-flex align-items-center rounded-pill ${styles.div_button} neumorphism `} style={{marginRight: "30%"}} >
+                        <Button theme={styles.btn_white} title="-" />
+                        <div style={{backgroundColor : "transparent"}}>1</div>
+                        <Button theme={styles.btn_black} title="+" />
+                    </div>
                 </div>
 
             </div>
