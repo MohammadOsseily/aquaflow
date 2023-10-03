df9a0ee796f96bb83cb8d178df4650643e88299d
MohammadOsseily <mohammad.osseily2@gmail.com>
2023-10-03 15:14:31 +0300

    finalized forgetpass page finalized login page finalized register page

---
+++ app/auth/forgetPass/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+import Image from 'next/image';
+function forgetPass() {
+    return (
+        <div className='flex h-screen '>
+            <div className='neumorphism m-auto w-full max-w-xs rounded bg-white p-5'>
+                <header>
+                    <Image
+                        className='mx-auto mb-5 w-20'
+                        src='/static/images/logo.png'
+                        alt='d'
+                        width={200}
+                        height={200}
+                    />
+                </header>
+                <form>
+                    <div>
+                        <label className='mb-2 block text-secondary' htmlFor='username'>
+                            Username
+                        </label>
+                        <input
+                            className='mb-6 w-full border-b-2 border-secondary p-2 text-secondary outline-none focus:bg-gray-300'
+                            type='text'
+                            name='username'
+                        />
+                    </div>
+
+                    <div>
+                        <input
+                            className='mb-6 w-full rounded-xl bg-secondary px-4 py-2 font-bold text-white hover:bg-neutral'
+                            type='submit'
+                        />
+                    </div>
+                </form>
+                <div>
+                    <a className='float-left text-sm text-secondary hover:text-neutral' href='/auth/login'>
+                        Login
+                    </a>
+                    <a className='float-right text-sm text-secondary hover:text-neutral' href='/auth/register'>
+                        Register
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default forgetPass;
