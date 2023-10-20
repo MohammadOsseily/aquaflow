e60c9656c0d515b28e6e8a8494e1247ee01fb0fa
MohammadOsseily <mohammad.osseily2@gmail.com>
2023-10-20 22:12:09 +0300

    added address page

--- app/account/addresses/page.tsx
+++ app/account/addresses/page.tsx
@@ -1,73 +1,32 @@
-import AccountBar from '../../components/AccountBar';
-import GlobalTextField from '../../components/GlobalTextField';
+import { PiTrashDuotone } from 'react-icons/pi';
+import { FiEdit2 } from 'react-icons/fi';
+
+import 'react-icons';
 
 function Addresses() {
     return (
         <div className=''>
-            <div className='pt-7 text-4xl'>Profile</div>
-            <div className='grid grid-cols-2 gap-4 pt-5'>
-                <AccountBar />
-                <div className=''>
-                    <GlobalTextField
-                        globalTextField={{
-                            inputAutoComplete: 'firstname',
-                            inputName: 'fName',
-                            inputPlaceholder: 'First Name',
-                            inputType: 'text',
-                        }}
-                    />
-                    <GlobalTextField
-                        globalTextField={{
-                            inputAutoComplete: 'lastname',
-                            inputName: 'lName',
-                            inputPlaceholder: 'Last Name',
-                            inputType: 'text',
-                        }}
-                    />
-                    <GlobalTextField
-                        globalTextField={{
-                            inputAutoComplete: 'displayName',
-                            inputName: 'displayName',
-                            inputPlaceholder: 'Display Name',
-                            inputType: 'text',
-                        }}
-                    />
-                    <GlobalTextField
-                        globalTextField={{
-                            inputAutoComplete: 'email',
-                            inputName: 'email',
-                            inputPlaceholder: 'Email',
-                            inputType: 'email',
-                        }}
-                    />
-                    <div className='pb-4 text-2xl'>Password Change</div>
-                    <GlobalTextField
-                        globalTextField={{
-                            inputAutoComplete: 'currentPassword',
-                            inputName: 'currentPassword',
-                            inputPlaceholder: 'Current password',
-                            inputType: 'password',
-                        }}
-                    />
-                    <GlobalTextField
-                        globalTextField={{
-                            inputAutoComplete: 'newPassword',
-                            inputName: 'newPassword',
-                            inputPlaceholder: 'New password',
-                            inputType: 'password',
-                        }}
-                    />
-                    <GlobalTextField
-                        globalTextField={{
-                            inputAutoComplete: 'confirmPassword',
-                            inputName: 'confirmPassword',
-                            inputPlaceholder: 'Confirm new password',
-                            inputType: 'password',
-                        }}
-                    />
-                    <button className='w-full rounded-full bg-secondary py-3 text-sm font-semibold uppercase text-white hover:bg-gray-600 '>
-                        Submit
-                    </button>
+            <div className='pb-7 pt-7 text-4xl'>Address</div>
+            <div className='grid-rows neumorphism grid bg-primary '>
+                <div className='mb-5 ml-2 mt-5 flex'>
+                    <div className='w-1/4 text-xs font-semibold uppercase text-gray-600'>Address Name</div>
+                    <div className='t w-1/4 text-center text-xs font-semibold uppercase text-gray-600'>Address</div>
+                    <div className='t w-1/4 text-center text-xs font-semibold uppercase text-gray-600'>City</div>
+                    <div className='w-1/4  text-center text-xs font-semibold uppercase text-gray-600'>Action</div>
+                </div>
+                <hr />
+                <div className='mb-5 ml-2 mt-5 flex'>
+                    <div className='w-1/4 text-xs font-semibold uppercase text-gray-600'>Mohammad Osseily</div>
+                    <div className='t w-1/4 text-center text-xs font-semibold uppercase text-gray-600'>Dahye</div>
+                    <div className='t w-1/4 text-center text-xs font-semibold uppercase text-gray-600'>Beirut</div>
+                    <div className='flex  w-1/4 justify-center text-center text-xs font-semibold uppercase text-gray-600'>
+                        <div className='mr-2 text-lg'>
+                            <PiTrashDuotone />
+                        </div>
+                        <div className='text-lg'>
+                            <FiEdit2 />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
