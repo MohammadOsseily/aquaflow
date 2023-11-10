f572fe1b927b2ec1c2010f1a6fb7c4bd6edd1051
MohammadOsseily <mohammad.osseily2@gmail.com>
2023-11-10 17:34:46 +0200

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className='  flex flex-col'>
+            <div className='flex flex-col'>
                 <Carsoule />
             </div>
             <ListOfProducts />
